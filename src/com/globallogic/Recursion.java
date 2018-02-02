ec964e92471c9df04f76e050aae61e8a88774881
alexandroid1 <alex123456>
2018-02-02 18:42:02 +0200

    Recursion

---
+++ src/com/globallogic/Recursion.java
@@ -0,0 +1,15 @@
+package com.globallogic;
+
+public class Recursion {
+    public static void main(String[] args) {
+        System.out.println(fact(5));
+    }
+
+    public static long fact(long n) {
+        if (n<=1)
+            return 1;
+        else
+            return n*fact(n-1);
+
+    }
+}
