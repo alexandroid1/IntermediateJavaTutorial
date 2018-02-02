cdec9f1a5d8d5083472d44b14b4afcdbda6ad1ce
alexandroid1 <alex123456>
2018-02-02 16:19:46 +0200

    1 - Common String Methods

---
+++ src/com/globallogic/CommonStringMethods.java
@@ -0,0 +1,18 @@
+package com.globallogic;
+
+public class CommonStringMethods {
+
+    public static void main(String[] args) {
+        String[] words = {"funk", "chunk", "furry", "baconator"};
+
+        for (String w: words){
+            if (w.startsWith("fu"))
+                System.out.println(w+" starts with fu");
+        }
+
+        for (String w: words){
+            if (w.endsWith("unk"))
+                System.out.println(w+" ends with unk");
+        }
+    }
+}
