498ff5d641fb4dc76b795fc99ddad930b693e2e7
alexandroid1 <alex123456>
2018-02-07 16:25:56 +0200

    CollectionsMethodSort

---
+++ src/com/globallogic/CollectionsMethodSort.java
@@ -0,0 +1,19 @@
+package com.globallogic;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class CollectionsMethodSort {
+    public static void main(String[] args) {
+        String[] crap = {"apples", "lemons", "geese", "bacon", "youtube"};
+        List<String> l1 = Arrays.asList(crap);
+
+        Collections.sort(l1);
+        System.out.printf("%s\n", l1);
+
+        Collections.sort(l1, Collections.reverseOrder());
+        System.out.printf("%s\n", l1);
+
+    }
+}
---
+++ src/com/globallogic/ConvertingListsToArrays.java
@@ -0,0 +1,21 @@
+package com.globallogic;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+public class ConvertingListsToArrays {
+    public static void main(String[] args) {
+        String[] stuff = {"babies", "watermelong", "melons", "fudge"};
+        LinkedList<String> thelist = new LinkedList<String>(Arrays.asList(stuff));
+
+        thelist.add("pumpikinf");
+        thelist.addFirst("firstthing");
+
+        // convert back to an array
+        stuff = thelist.toArray(new String[thelist.size()]);
+
+        for(String x : stuff)
+            System.out.printf("%s ", x);
+
+    }
+}
