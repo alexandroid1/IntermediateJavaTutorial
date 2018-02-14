8dafff943cfa744800223d7442654b2cc4a17e93
alexandroid1 <alex123456>
2018-02-14 18:00:49 +0200

    Stacks Push Pop

---
+++ src/com/globallogic/stacks/StacksPushPop.java
@@ -0,0 +1,30 @@
+package com.globallogic.stacks;
+
+import java.util.Stack;
+
+public class StacksPushPop {
+    public static void main(String[] args) {
+
+        Stack<String> stack = new Stack<String>();
+        stack.push("bottom");
+        printStack(stack);
+        stack.push("second");
+        printStack(stack);
+        stack.push("third");
+        printStack(stack);
+
+        stack.pop();
+        printStack(stack);
+        stack.pop();
+        printStack(stack);
+        stack.pop();
+        printStack(stack);
+    }
+
+    private static void printStack(Stack<String> s){
+        if(s.isEmpty())
+            System.out.println("You have nothing in your stack");
+        else
+            System.out.printf("%s TOP\n", s);
+    }
+}
