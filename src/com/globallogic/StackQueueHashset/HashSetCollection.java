2de6f9b1c26f8d12c5d0282f1a219b589a01af44
alexandroid1 <alex123456>
2018-02-15 14:14:20 +0200

    HashSet Collection

---
+++ src/com/globallogic/StackQueueHashset/HashSetCollection.java
@@ -0,0 +1,25 @@
+package com.globallogic.StackQueueHashset;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class HashSetCollection {
+    public static void main(String[] args) {
+        String[] things = {"apple", "bob", "ham", "bob", "bacon"};
+        List<String> list = Arrays.asList(things);
+
+        System.out.printf("%s ", list);
+        System.out.println();
+
+        Set<String> set = new HashSet<String>(list);
+        System.out.printf("%s ", set);
+
+    }
+}
+
+
+
+
+
